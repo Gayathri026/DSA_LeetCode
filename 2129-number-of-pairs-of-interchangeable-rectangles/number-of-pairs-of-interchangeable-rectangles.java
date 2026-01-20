1a4ec1135edaa2266a75c36ded856ed5983fd035
Gayathri R <157454384+Gayathri026@users.noreply.github.com>
2026-01-20 21:23:35 +0530

    Time: 44 ms (53.87%) | Memory: 143.3 MB (85.31%) - LeetSync

--- 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.java
+++ 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.java
@@ -1,20 +1,13 @@
- class Solution {
-    public long interchangeableRectangles(int[][] rectangles) {
+class Solution {
+    public long interchangeableRectangles(int[][] rec) {
         
-        HashMap<Double, Long> map = new HashMap<>();
-        long count = 0;
-		
-        for (int[] A : rectangles) {
-		
-            double x = A[0];
-            double y = A[1];
-            double division = (double) x / y;
-			
-            if (map.containsKey(division)) {
-                count += map.get(division);
-            }
-            map.put(division, map.getOrDefault(division, 0L) + 1);
-        }
-        return count;
+        int i,j,n=rec.length;
+        long sum=0;
+        Map<Double,Integer> map=new HashMap();
+        for(i=0;i<n;i++)
+            map.put((double)rec[i][0]/rec[i][1],map.getOrDefault((double)rec[i][0]/rec[i][1],0)+1);
+        for(Double key : map.keySet())
+            sum+=(long)map.get(key)*(map.get(key)-1)/2;
+        return sum;
     }
 }
