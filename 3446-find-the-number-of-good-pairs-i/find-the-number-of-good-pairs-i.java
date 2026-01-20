ae817e4be62724de864ad891f0d0f87096180149
Gayathri R <157454384+Gayathri026@users.noreply.github.com>
2026-01-20 23:04:33 +0530

    Time: 2 ms (4.45%) | Memory: 44.9 MB (8.35%) - LeetSync

--- 3446-find-the-number-of-good-pairs-i/find-the-number-of-good-pairs-i.java
+++ 3446-find-the-number-of-good-pairs-i/find-the-number-of-good-pairs-i.java
@@ -1,9 +1,21 @@
 class Solution {
     public int numberOfPairs(int[] nums1, int[] nums2, int k) {
-        int count=0;
-        for(int i=0; i<nums1.length; i++){
-            for(int j=0; j<nums2.length; j++){
-                if(nums1[i]%(nums2[j]*k) == 0) count++;
+        int count =0;
+        HashMap <Integer, Integer>check = new HashMap<>(); 
+        for(int num : nums2){
+            check.put(num, check.getOrDefault(num, 0)+1);
+        }
+        for(int num : nums1){
+            for (int d = 1; d*d <= num; d++) {
+                if (num%d == 0) {
+                    if (d%k == 0 && check.containsKey(d/k)) {
+                        count += check.get(d/k);
+                    }
+                    int other = num/d;
+                    if (other!=d && other % k==0 && check.containsKey(other/k)) {
+                        count += check.get(other/k);
+                    }
+                }
             }
         }
         return count;
