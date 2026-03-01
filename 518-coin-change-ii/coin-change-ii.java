d076962f74eddecbd69f5c403e2721b875e8e6a7
Gayathri R <157454384+Gayathri026@users.noreply.github.com>
2026-03-01 12:41:10 +0530

    Time: 15 ms (40.32%) | Memory: 57.2 MB (33.41%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,17 +1,29 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n + 1][amount + 1];
-        for (int i = 0; i <= n; i++) dp[i][0] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= amount; j++) {
-                if (coins[i - 1] <= j) {      
-                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
+        int[][] dp = new int[n][amount + 1];
+
+        for(int i = 0; i < n; i++){
+            Arrays.fill(dp[i], -1);
         }
-        return dp[n][amount];
+
+        return helper(n - 1, amount, coins, dp);
+    }
+
+    private int helper(int ind, int t, int[] coins, int[][] dp) {
+        if (ind == 0) {
+            return (t % coins[0] == 0) ? 1 : 0;
+        }
+
+        if (dp[ind][t] != -1)
+            return dp[ind][t];
+
+        int notTake = helper(ind - 1, t, coins, dp);
+
+        int take = 0;
+        if (coins[ind] <= t)
+            take = helper(ind, t - coins[ind], coins, dp);
+
+        return dp[ind][t] = notTake + take;
     }
 }
