d296bb1126298311530a0cc9d1a4f4f631bfde2e
Gayathri R <157454384+Gayathri026@users.noreply.github.com>
2026-03-13 22:16:44 +0530

    Time: 1 ms (86.18%) | Memory: 42.7 MB (96.91%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,23 +1,27 @@
 class Solution {
     public int numDecodings(String s) {
         int n = s.length();
-        int[] dp = new int[n + 1];
+        
+        if(s.charAt(0) == '0') return 0;
 
-        dp[0] = 1;
-        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        int prev2 = 1; // dp[i-2]
+        int prev1 = 1; // dp[i-1]
 
-        for(int i = 2; i <= n; i++) {
+        for(int i = 1; i < n; i++) {
 
-            int oneDigit = s.charAt(i-1) - '0';
-            int twoDigit = Integer.parseInt(s.substring(i-2, i));
+            int curr = 0;
 
-            if(oneDigit >= 1)
-                dp[i] += dp[i-1];
+            if(s.charAt(i) != '0')
+                curr += prev1;
 
+            int twoDigit = (s.charAt(i-1)-'0')*10 + (s.charAt(i)-'0');
             if(twoDigit >= 10 && twoDigit <= 26)
-                dp[i] += dp[i-2];
+                curr += prev2;
+
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        return dp[n];
+        return prev1;
     }
 }
